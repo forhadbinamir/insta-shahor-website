fd472e19bb7e49f895ee02853fb2a51e2c7833de
forhadbinamir <forhadbinamir@gmail.com>
2022-03-11 03:16:27 +0300

    forth problem solved alhamdulillah

--- js/app.js
+++ js/app.js
@@ -83,7 +83,6 @@ const createPost = (post) => {
                     alt="Post Content"
                   />
                   <div>
-                  <p>${displayContent(post.description)}</p>
                   </div>
                 </div>
                 
@@ -126,9 +125,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
